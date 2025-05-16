dfe0e04c8d0488bdd4584cfc6b1605b1da7b2354
Harshit Sharma <harshit.belovance@gmail.com>
2025-05-17 01:17:55 +0530

    splash screen first commit

--- lib/widgets/custom_image.dart
+++ lib/widgets/custom_image.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CustomImage {
-  static customImage(String img) {
+  static customImage({required String img}) {
     return Image.asset("assets/images/$img", fit: BoxFit.cover);
   }
 }
