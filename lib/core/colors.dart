918df62612a34e116d39d02ad27cd825d4bf3567
Harshit Sharma <harshit.belovance@gmail.com>
2025-05-17 14:13:17 +0530

    Login screen ui done.

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color scaffoldBackgroundColor = Color(0xFFF7CB45);
+  static const Color fadeColor = Color(0xFF9C9C9C);
+  static const Color linkColor = Color(0xFF269237);
 }
